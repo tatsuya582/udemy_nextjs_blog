8bd0bf5388aa827490a300d331c59ddc7ff422a0
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-09-17 18:39:26 +0900

    add: トップページの雛形を作成

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -5,8 +5,84 @@ import styles from "@/styles/Home.module.css";
 import Link from "next/link";
 import Layout from "../components/Layout";
 
+import utilStyle from "../styles/utils.module.css";
+
 export default function Home() {
   return (
-    <Layout></Layout>
+    <Layout>
+      <section className={utilStyle.headingMd}>
+        <p>
+          私はフルスタックエンジニアです/Udemy講師として活動しています/好きな言語はJavaScriptです
+        </p>
+      </section>
+
+      <section className={`${utilStyle.headingMd} ${utilStyle.padding1px}`}>
+        <h2>📝エンジニアのブログ</h2>
+        <div className={styles.grid}>
+          <article>
+            <Link href="/">
+              <img 
+                src="/images/thumbnail01.jpg" 
+                className={styles.thumbnailImage}
+              />
+            </Link>
+            <Link href="/">
+              <div className={utilStyle.boldText}>SSGとSSRの使い分けの場面はいつなのか？</div>
+            </Link>
+            <br />
+            <small className={utilStyle.lightText}>
+              Februay 23, 2020
+            </small>
+          </article>
+          <article>
+            <Link href="/">
+              <img 
+                src="/images/thumbnail01.jpg" 
+                className={styles.thumbnailImage}
+              />
+            </Link>
+            <Link href="/">
+              <div className={utilStyle.boldText}>SSGとSSRの使い分けの場面はいつなのか？</div>
+            </Link>
+            <br />
+            <small className={utilStyle.lightText}>
+              Februay 23, 2020
+            </small>
+          </article>
+          <article>
+            <Link href="/">
+              <img 
+                src="/images/thumbnail01.jpg" 
+                className={styles.thumbnailImage}
+              />
+            </Link>
+            <Link href="/">
+              <div className={utilStyle.boldText}>SSGとSSRの使い分けの場面はいつなのか？</div>
+            </Link>
+            <br />
+            <small className={utilStyle.lightText}>
+              Februay 23, 2020
+            </small>
+          </article>
+          <article>
+            <Link href="/">
+              <img 
+                src="/images/thumbnail01.jpg" 
+                className={styles.thumbnailImage}
+              />
+            </Link>
+            <Link href="/">
+              <div className={utilStyle.boldText}>SSGとSSRの使い分けの場面はいつなのか？</div>
+            </Link>
+            <br />
+            <small className={utilStyle.lightText}>
+              Februay 23, 2020
+            </small>
+          </article>
+        </div>
+      </section>
+
+      
+    </Layout>
   );
 }
